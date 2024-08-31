6dc65633d124052fe45a1453ba3d72250e08d669
Guilherme Kunigami <kunigami@fb.com>
2024-08-31 10:23:55 -0700

    improve merge script

--- tools/merge.sh
+++ tools/merge.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# TODO: check current branch is not master
-git reset $(git merge-base master $(git branch --show-current))
-git add -A
-
-echo "Creating new, clean commit"
-git commit -m "new post: $(git rev-parse --abbrev-ref HEAD)$"
-
-# Save current branch to merge later
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-git checkout master
+if [ "$current_branch" == "master" ]; then
+    echo "Currently on $current_branch branch. Skipping..."
+else
+    git reset $(git merge-base master $(git branch --show-current))
+    git add -A
+    git commit -m "new post: $(git rev-parse --abbrev-ref HEAD)$"
+    git checkout master
+
+    echo "Do you want to continue? (y/N)"
+    read -r response
+    if [[ "$response" == "y" || "$response" == "Y" ]]; then
+        git merge $current_branch
+    else
+        echo "Skip running: git merge $current_branch"
+    fi
+fi
